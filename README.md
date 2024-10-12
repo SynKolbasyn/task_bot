bca51a03a6d73ae4a3a0474e05b26e8ec380d1f8
Andrew Kozmin <syn.kolbasyn.06@gmail.com>
2024-10-12 10:50:22 +0300

    feat: License added

---
+++ README.md
@@ -0,0 +1,13 @@
+# Task bot
+
+## Cloning
+```Bash
+git clone --recurse-submodules https://github.com/SynKolbasyn/task_bot.git
+```
+
+
+## Updating
+```Bash
+git pull --recurse-submodules
+git submodule update --remote
+```
--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,29 @@
+/**
+ * Task bot. A bot for issuing tasks and sending them for verification.
+ * Copyright (C) 2024  Andrew Kozmin <syn.kolbasyn.06@gmail.com>
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
 }
